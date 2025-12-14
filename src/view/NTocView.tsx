89d70a7ecc186d14215991d1a2490eb6a0afba3a
RavenHogwarts <fengyj120@163.com>
2025-12-14 19:27:30 +0800

    Async method 'onClose' has no 'await' expression.

--- src/view/NTocView.tsx
+++ src/view/NTocView.tsx
@@ -91,10 +91,11 @@ export class NTocView extends ItemView {
 		);
 	}
 
-	protected async onClose(): Promise<void> {
+	async onClose(): Promise<void> {
 		if (this.root) {
 			this.root.unmount();
 			this.root = null;
 		}
+		await super.onClose();
 	}
 }
